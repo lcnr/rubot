75b4327d56f6ed68f96bff4634046cd7aae367e3
lcnr/Bastian Kauschke <bastian_kauschke@hotmail.de>
2019-04-20 14:36:14 +0200

    fix chess fitness calculation

--- examples/chess.rs
+++ examples/chess.rs
@@ -1,7 +1,7 @@
 use std::io;
 use std::time::Duration;
 
-use shakmaty::{uci::Uci, Color, Move, MoveList, Position, Role, Setup};
+use shakmaty::{uci::Uci, Color, Move, MoveList, Outcome, Position, Role, Setup};
 
 /// this example requires a newtype due to orphan rules, as both shakmaty::Chess and rubot::Game
 /// are from outside of this example.
@@ -23,25 +23,38 @@ impl rubot::Game for Chess {
     fn execute(&mut self, action: &Self::Action, player: &Self::Player) -> Self::Fitness {
         self.0.play_unchecked(action);
 
-        let mut fitness = 0;
-        for (_square, piece) in self.0.board().pieces() {
-            // values based on https://medium.freecodecamp.org/simple-chess-ai-step-by-step-1d55a9266977
-            let value = match piece.role {
-                Role::Pawn => 10,
-                Role::Knight => 30,
-                Role::Bishop => 30,
-                Role::Rook => 50,
-                Role::Queen => 90,
-                Role::King => 900,
-            };
-
-            if piece.color == *player {
-                fitness += value;
-            } else {
-                fitness -= value;
+        if let Some(outcome) = self.0.outcome() {
+            match outcome {
+                Outcome::Draw => 0,
+                Outcome::Decisive { winner } => {
+                    if winner == *player {
+                        std::i32::MAX
+                    } else {
+                        std::i32::MIN
+                    }
+                }
             }
+        } else {
+            let mut fitness = 0;
+            for (_square, piece) in self.0.board().pieces() {
+                // values based on https://medium.freecodecamp.org/simple-chess-ai-step-by-step-1d55a9266977
+                let value = match piece.role {
+                    Role::Pawn => 10,
+                    Role::Knight => 30,
+                    Role::Bishop => 30,
+                    Role::Rook => 50,
+                    Role::Queen => 90,
+                    Role::King => 900,
+                };
+
+                if piece.color == *player {
+                    fitness += value;
+                } else {
+                    fitness -= value;
+                }
+            }
+            fitness
         }
-        fitness
     }
 }
 
