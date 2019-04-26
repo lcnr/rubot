d4d57220027f55e9c9c2e4c0b24f126438e99bdb
lcnr/Bastian Kauschke <bastian_kauschke@hotmail.de>
2019-04-26 15:58:16 +0200

    add logger test

--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -1,5 +1,42 @@
-use crate::{tree::Node, Bot};
+use crate::{tree::Node, Bot, Depth, Logger, Steps, ToCompletion};
 
 mod brute;
 mod completed;
 mod partial;
+
+#[rustfmt::skip]
+const LOGGER_EQ: Node = Node::root().children(&[
+    Node::new(false, 0).children(&[
+        Node::new(false, 0).children(&[
+            Node::new(false, 0).children(&[
+                Node::new(false, 0).children(&[
+                    Node::new(false, 0),
+                ]),
+            ]),
+        ]),
+    ]),
+    Node::new(false, 0).children(&[
+        Node::new(true, 5).children(&[
+            Node::new(true, 5).children(&[
+                Node::new(true, 3),
+            ]),
+        ]),
+    ]),
+    Node::new(false, 0).children(&[
+        Node::new(false, 4),
+        Node::new(false, 2).children(&[
+            Node::new(false, 2),
+        ]),
+    ]),
+]);
+
+#[test]
+fn logger_eq() {
+    let mut logger = Logger::new(Steps(7));
+    Bot::new(true).select(&LOGGER_EQ, &mut logger);
+    assert_eq!(logger.steps(), 7);
+
+    let mut logger = Logger::new(Depth(2));
+    Bot::new(true).select(&LOGGER_EQ, &mut logger);
+    assert_eq!(logger.depth(), 2);
+}
